e84b9016f4b86eff625fec7af375b7426dad6e25
= <=>
2021-08-13 09:25:27 +0700

    added router-link to about page and manage page

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,23 +4,26 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-4xl mr-2 mt-0" href="#">Music</a>
+      <router-link class="text-white font-bold uppercase text-4xl mr-2 mt-0" :to="{ name: 'home' }" exact-active-class="no-active">Music</router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class=" px-2 text-white fs mr-0 ml-5 m-2" :to="{ name: 'about' }">About</router-link>
+          </li>
           <li v-if="!userLoggedIn">
-            <a class="text-pink-400 px-2 text-white fs ml-6" href="#" @click.prevent="toggleAuthModal">
+            <a class=" px-2 text-white fs ml-6" href="#" @click.prevent="toggleAuthModal">
                Login / Register
                 </a>
           </li>
           <template v-else>
           <li>
-            <a class="text-pink-400 px-2 text-white fs mr-0 ml-5 m-2" href="#">Manage</a>
+            <router-link class=" px-2 text-white fs mr-0 ml-5 m-2" :to="{ name: 'manage' }">Manage</router-link>
           </li>
           <li>
-            <a class="text-pink-400 px-4 text-white fs" href="#" @click.prevent="signout">Log Out</a>
+            <a class=" px-4 text-white fs" href="#" @click.prevent="signout">Log Out</a>
           </li>
           </template>
         </ul>
